5446a20fff2a0f380f71b2b9ac908ddb4b2362ed
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-11 11:34:01 +0900

    [Silver IV] Title: 가로수, Time: 4088 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,22 +1,20 @@
 n = int(input())
-lst = []
-max_n = 1
+gap = []
 answer = 0
 def gcd(x, y):
     while y != 0:
         x, y = y, x % y
     return x
-
-for i in range(n):
-    a = int(input())
-    lst.append(a)
-    if i == 1:
-        max_n = a - b
-    elif i > 1:
-        max_n = gcd(max_n, a - b)
+b = int(input())
+for i in range(n -1):
+    a = int(input())    
+    gap.append(a - b)   
     b = a
-for idx, l in enumerate(lst):
-    if idx < len(lst)-1:
-        diff = lst[idx + 1] - l
-        answer += int((diff / max_n) - 1)
+
+max_n = gap[0]
+for j in range(1, len(gap)):
+    max_n = gcd(max_n, gap[j])
+for g in gap:
+    answer += ((g // max_n) - 1)
+
 print(answer)
