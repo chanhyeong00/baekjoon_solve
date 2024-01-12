d6f84ecaad001c0e87c043272aca0f4345df3880
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-12 17:34:36 +0900

    [Silver IV] Title: 제로, Time: 92 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,12 @@
-k = int(input())
+from sys import stdin
+
+k = int(stdin.readline())
+
 lst = []
 answer = 0
 
 for _ in range(k):
-    num = int(input())
+    num = int(stdin.readline())
     if num != 0:
         lst.append(num)
         answer += num
