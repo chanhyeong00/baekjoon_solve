693b6770fd612172687a89d4a1678ec123197009
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-18 10:33:14 +0900

    Update 영단어 암기는 괴로워.py

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -11,10 +11,12 @@ for _ in range(n):
               note[word] += 1
 
 answer = list(note.items())
-
+# 하나씩 풀어쓸 땐 우선순위 가장 낮은 애부터 sort
 answer.sort(key=lambda x : x[0])
 answer.sort(key=lambda x : len(x[0]), reverse=True)
 answer.sort(key=lambda x: x[1], reverse=True)
 
+#answer.sort(key=lambda x: (-x[1], -len(x[0]), x[0]))
+# 한번에 쓰는 것도 가능(우선순위 순서대로)
 for a in answer:
      print(a[0])
