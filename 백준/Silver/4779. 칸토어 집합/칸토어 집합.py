cc496746ddc6ea46ceee7d14690c86b2ecedda22
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-22 10:35:19 +0900

    [Silver III] Title: 칸토어 집합, Time: 48 ms, Memory: 32500 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,19 +1,13 @@
 from sys import stdin
 
-def cantorian(start, n):
-
-    if n == 1: # 길이가 1이 되면
-        return
-    for i in range(start + n//3, start + n//3 * 2):
-        answer[i] = ' '
-    cantorian(start, n // 3)
-    cantorian(start + n // 3 * 2, n//3)
-
+def cantorian(n):
+    if n == 1: # 길이가 1이 된 경우
+        return '-'
+    else: # 아니면 3등분
+        return cantorian(n // 3) + ' ' * (n // 3) + cantorian(n // 3)
 while True:
-    try: # 입력값 없으면 탈출
+    try:
         n = int(stdin.readline())
-        answer = ['-'] * (3**n)
-        cantorian(0, 3**n)
-        print(''.join(answer))
+        print(cantorian(3**n))
     except:
         break
