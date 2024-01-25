7e28e0b73486d3c7b8517d25edbb75ba54f8646f
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-25 10:36:31 +0900

    Update 백트레킹 기본 구조.py

--- 백준/백트레킹 기본 구조.py
+++ 백준/백트레킹 기본 구조.py
@@ -3,15 +3,15 @@ arr = []
 
 
 def make_arr(k, n, m):
-    if k == m:
+    if k == m: # 일정 깊이에 도착하면 동작 후 리턴
         print(' '.join(map(str, arr)))
         return
-    for i in range(len(visited)):
+    for i in range(len(visited)): # 이 부분에서 탐색시작 지점을 잡고 시간복잡도를 줄이는 게 관건.
         if not visited[i]:
-            visited[i] = True
-            arr.append(i+1)
-            make_arr(k+1, n, m)
-            visited[i] = False
+            visited[i] = True # 방문함을 표시
+            arr.append(i+1) 
+            make_arr(k+1, n, m) # 백트레킹
+            visited[i] = False # 탐색끝
             arr.pop()
 
 
