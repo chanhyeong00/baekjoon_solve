a569a2154ac1b9ac1b93a1b1bb19340d922775bf
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-30 15:34:21 +0900

    Update 정수 삼각형.py

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -11,9 +11,14 @@ for i in range(n):
 
 for i in range(1, n): # 층
     for j in range(i+1): # 이전값의 합 + 고정값
-        if j == 0: dp[i][j] = dp[i-1][j] + tri[i][j]
-        elif j == i: dp[i][j] = dp[i-1][j-1] + tri[i][j]
+        if j == 0: dp[i][j] = dp[i-1][j] + tri[i][j] # 맨 왼쪽은 하나밖에 없음
+        elif j == i: dp[i][j] = dp[i-1][j-1] + tri[i][j] # 맨 오른쪽도 하나밖에 없음
         else: dp[i][j] = max(dp[i-1][j] + tri[i][j], dp[i-1][j-1] + tri[i][j])
+        # 나머지는 왼쪽 오른쪽 살펴서 최대값을 갱신
         # 그 층까지의 합중 최대값
 
 print(max(dp[n-1]))
+
+
+# 이것도 똑같이 마지막 값을 고정하고 
+# 이전까지의 최대값 + 마지막값(고정시킨) 이라는 점화식으로 풀었음
