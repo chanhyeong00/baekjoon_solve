52d3287fe7ebcf23e540a06be674ba729763dd7d
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-01-31 11:11:11 +0900

    Update 계단 오르기.py

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -14,8 +14,11 @@ def up_stair(floor):
     if mem[floor] != -1: # 이미 계산된 값 있으면
         return mem[floor]
     
-    mem[floor] = max(stair[floor] + up_stair(floor-2), stair[floor] + stair[floor-1] + up_stair(floor-3))
-
+    mem[floor] = max(stair[floor] + up_stair(floor-2),\
+                     stair[floor] + stair[floor-1] + up_stair(floor-3))
+    # 한 계단을 밟으면 두가지 경우만 가능함
+    # 아래로 2칸 내려가거나
+    # 한 칸 내려간 경우는 다음에 두칸 내려가야함
     return mem[floor]
 
 result = up_stair(n-1)
