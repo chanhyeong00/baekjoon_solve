661569c42417de022e18e65e8228bd2faa5d2885
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-01 11:26:50 +0900

    Update 포도주 시식.py

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -13,4 +13,9 @@ if n > 1:
 for i in range(2, n):
     dp[i] = max(grape[i] + grape[i-1] + dp[i-3],\
                 grape[i] + dp[i-2], dp[i-1])
+
+# 한 잔을 기준으로 
+# 그 잔을 마시고 다음잔, 다다음잔 먹는 경우
+# 그 잔을 마시고 다담잔 마시는 경우
+# 그 잔 안 마시고 다음잔 먹는 경우로 총 3개
 print(dp[n-1])
