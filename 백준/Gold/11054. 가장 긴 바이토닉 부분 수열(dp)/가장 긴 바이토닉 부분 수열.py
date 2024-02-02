8ee454443ec26bb4df7d69a4b93e010ce28660d5
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-02 17:11:01 +0900

    Update 가장 긴 바이토닉 부분 수열.py

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열(dp)/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열(dp)/가장 긴 바이토닉 부분 수열.py
@@ -6,12 +6,12 @@ dp_left = [1] * n
 dp_right = [1] * n
 answer = 0 
 
-for i in range(n): # 기준 i로 왼쪽 방향 감소하는 수열 길이 계산
+for i in range(n): # 기준 i를 기준으로 왼쪽 방향으로 증가하는 수열의 최대길이 구하기 위함(완->오 감소)
     for j in range(i):
         if a[j] < a[i]:
             dp_left[i] = max(dp_left[i], dp_left[j]+1)
             # 기준에 따른 수열의 최대개수 계산
-for i in range(n-1, -1, -1): # 기준 i로 오른쪽 방향 계산
+for i in range(n-1, -1, -1): # 기준 i를 기준으로 오른쪽 방향으로 증가하는 (오->왼 감소)
     for j in range(i, n):
         if a[j] < a[i]:
             dp_right[i] = max(dp_right[i], dp_right[j]+1)
