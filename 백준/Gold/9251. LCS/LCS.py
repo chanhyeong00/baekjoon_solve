c5fdb09049405bbc0f02f727112fb5c7ed7fca3b
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-05 17:04:28 +0900

    [Gold V] Title: LCS, Time: 460 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -6,14 +6,15 @@ s2 = stdin.readline().strip()
 l1 = len(s1)
 l2 = len(s2)
 
-dp = [[0] * (l2+1) for _ in range(l1+1)] # s1 을 한 글자씩 s2 전체와 비교
-# 초기값을 위해 +1 더 해준 것
+dp1 = [0 for _ in range(l2+1)] # lcs 길이 기록
+dp2 = [0 for _ in range(l2+1)]
 
-for i in range(1, l1+1): # 
+for i in range(l1): # 
+    dp2 = dp1.copy()
     for j in range(1, l2+1):
-        if s1[i-1] == s2[j-1]: # 두 문자열이 같으면
-            dp[i][j] = dp[i-1][j-1] + 1 # 이전까지의 lcs 값에서 1을 늘림
-        else: # 다른 경우는 이전까지의 lcs 최대값 저장(2가지 경우 -> 위, 왼쪽)
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        
-print(dp[-1][-1])
+        if s1[i] == s2[j-1]: # 두 문자열이 같으면
+            dp1[j] = dp2[j-1] + 1 # 이전까지의 lcs 값에서 1을 늘림
+        else: # 다르면 이전값 저장
+            dp1[j] = max(dp1[j-1], dp1[j])
+    #dp2 = dp1 # 참조하기 때문에 쓰지말기
+print(dp1[-1])
