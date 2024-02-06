d0ca02cbe40dba92510cc6b6b5027a32dafb2fbc
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-06 17:46:47 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,24 +1,10 @@
 from sys import stdin
 read = stdin.readline
-
-def devide(num):
-    if num == 0 or num == 1: # 끝까지 도달
-        return 1
-    if num == 2: # 2면 2가지로 나눠짐(1+1, 2)
-        return 2
-    if dp[num] != -1: # 이미 값 있으면 있는 값 씀
-        return dp[num]
-    else: # 3부터는 3가지
-        dp[num] = devide(num-1) + devide(num-2) + devide(num-3)
-
-    return dp[num]
-    
-
+   
+dp = [0] * (11)
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 11):
+    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 t = int(read())
 for _ in range(t):
-    num = int(read())
-    dp = [-1] * (num+1)
-    dp
-    result = devide(num)
-    print(result)
-
+    print(dp[int(read())])
