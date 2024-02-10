052892a5a3df87895c27c3ea76e39791dd845e51
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-10 20:05:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1392 ms, Memory: 148076 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,25 @@
+from sys import stdin
+read = stdin.readline
+
+n, m = map(int, read().split())
+lst = []
+
+dp_row = [[0] * (n+1) for _ in range(n+1)]  # 행 별 누적합
+dp_sq = [[0] * (n+1) for _ in range(n+1)] # 사각형 누적합
+
+
+for _ in range(n):
+    lst.append(list(map(int, read().split())))
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp_row[i][j] = dp_row[i][j-1] + lst[i-1][j-1]
+
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp_sq[i][j] = dp_sq[i-1][j] + dp_row[i][j-1] + lst[i-1][j-1]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, read().split())
+    print(dp_sq[x2][y2] - (dp_sq[x1-1][y2] + dp_sq[x2][y1-1] - dp_sq[x1-1][y1-1]))
