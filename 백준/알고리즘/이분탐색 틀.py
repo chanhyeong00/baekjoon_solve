5275b6c4af35596e50dc8ce11ca8edaa139133f5
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-15 15:35:49 +0900

    Create 이분탐색 틀.py

---
+++ 백준/알고리즘/이분탐색 틀.py
@@ -0,0 +1,16 @@
+def binary_search(target, data):
+    data.sort()
+    start = 0 			# 맨 처음 위치
+    end = len(data) - 1 	# 맨 마지막 위치
+
+    while start <= end:
+        mid = (start + end) // 2 # 중간값
+
+        if data[mid] == target:
+            return mid 		# target 위치 반환
+
+        elif data[mid] > target: # target이 작으면 왼쪽을 더 탐색
+            end = mid - 1
+        else:                    # target이 크면 오른쪽을 더 탐색
+            start = mid + 1
+    return
