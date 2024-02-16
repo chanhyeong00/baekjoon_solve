002528b8873f37065e34943055284f9822dac32d
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-16 16:56:42 +0900

    [Silver III] Title: 주유소, Time: 148 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver III/13305. 주유소/주유소.py
+++ 백준/Silver III/13305. 주유소/주유소.py
@@ -6,25 +6,19 @@ n = int(input())
 distances = list(map(int, input().split()))
 oil = list(map(int, input().split()))
 
-inf = []
-check_pt = [0] * n
-oil_l = oil[0] * distances[0] # 처음엔 무조건 주유
+dp = [0] * n # 최소값 표시
+min_ = 10 ** 9
+for i in range(n):
+    if oil[i] < min_:
+        min_ = oil[i]
+        dp[i] = min_
+check = 0
 
-for i in range(1, n-1): # 인덱스, 가격
-    inf.append((i, oil[i]))
 
-inf.sort(key= lambda x:[x[1], x[0]]) # 가격순 정렬
-
-pt, price = n, 10**9 # 특정 지점과 지불해야할 가격
-
-for i in inf: # 정보를 돈다(가격순 정렬된 상태)
-    if i[0] <= pt: # 특정 지점보다 앞에 있으면
-        check_pt[i[0]] = i[1] # 그 지점보다 앞은 이 가격으로 정하고
-        pt = i[0] # 포인트 표시
-        price = min(price, i[1]) # 기존 가격과 비교
-    else: # 특정 포인트 뒤에 것들은 최소값으로 표시
-        check_pt[i[0]] = price
+answer = 0
+min_ = 10 ** 9
 for i in range(n-1):
-   oil_l += (distances[i] * check_pt[i])
-
-print(oil_l)
+    if dp[i] != 0:
+        min_ = min(min_, dp[i])
+    answer += (min_ * distances[i])
+print(answer)
