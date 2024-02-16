3e2c9683fa79d7a703a09544ccf5d15b3176df61
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-16 15:30:02 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver II/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver II/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -2,28 +2,15 @@ from sys import stdin
 
 input = stdin.readline
 
-form =  input() # -가 나오면 괄호 열고, -나 맨 끝이면 괄호 닫고를 반복한다.
-s = 0
-flag = False
-num = ''
-for idx, f in enumerate(form):
-    if flag == True: # 괄호 열린 상태
-        if idx == len(form) - 1:
-            s -= int(num)
-        elif f == '-' or f == '+':
-            s -= int(num)
-            num = ''
-        else:
-            num += f
-    else: # 괄호 닫힌 상태
-        if idx == len(form) - 1:
-            s += int(num)
-        elif f == '-' or f == '+': # 이때부턴 나오는 수 전부 뺌
-            s += int(num)
-            if f == '-':
-                flag = True
-            num = ''
-        else: # 연산자 아니면 숫자 이어붙임
-            num += f
+form =  input().strip().split('-') # -가 나온 순간부터 다 뻄
 
-print(s)
+answer = 0
+
+for i in form[0].split('+'):
+    answer += int(i)
+
+for i in range(1, len(form)):
+    for j in form[i].split('+'):
+        answer -= int(j)
+
+print(answer)
