80c111c9a95ef17e7ce95d0e3a4db36321c7095e
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-02-19 11:30:11 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -4,14 +4,4 @@ input = stdin.readline
 
 a, b, c = map(int, input().split())
 
-def multi(a, divide):
-    if divide == 1: # 거듭제곱 1
-        return a % c
-    else: # 아니면
-        part_num = multi(a, divide//2)
-        if divide % 2 == 0: # 2의 거듭제곱이면
-            return (part_num **2) % c
-        else:
-            return (part_num ** 2 * a) % c
-
-print(multi(a, b))
+print(pow(a,b,c))
