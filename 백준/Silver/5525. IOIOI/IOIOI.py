d81f0abdb866a56912e71a6b6d4814169fc1dfb9
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2024-03-15 16:52:33 +0900

    [Silver I] Title: IOIOI, Time: 380 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -4,23 +4,23 @@ input = stdin.readline
 
 n = int(input())
 m = int(input())
-
 S = input()
-n_p = 2 * n + 1
+
+pattern = "IOI"
 
 answer = 0
-change = {'I':'O', 'O':'I'}
-for i in range(m):
-    k = i
-    if S[i] == 'I':
-        curr = 'I'
-        if i+n_p -1 < m:
-            for j in range(i+n_p -1, -1, -1):
-                if k == j and S[k] == S[j] and S[k]==curr:
-                    answer += 1
-                    break
-                if S[k] != S[j] or curr != S[k]:
-                    break       
-                curr = change[curr] 
-                k += 1
+num_pat = 0
+end = m
+for i in range(m-2): #패턴 IOI이므로 3칸씩 봄
+
+    if end < i: # 패턴 아예 사라짐
+        num_pat = 0
+
+    if S[i:i+3] == pattern: # 패턴이 있다면
+        end = i+2
+        num_pat += 1 # 부분패턴 개수 세고
+        if num_pat == n: # 원하는 길이라면
+            answer += 1 #정답
+            num_pat -= 1 # 앞의 IO 패턴 하나를 줄인다.
+
 print(answer)
