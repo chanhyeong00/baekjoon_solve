36d01ff5cc62d266367fc168028d54ee8e93bd1e
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-02-25 05:33:55 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,25 +1,31 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
 while True:
-     open_state = []# 0은 소괄호, 1은 대괄호
-     sen = stdin.readline().replace("\n", "")
-     if sen == '.':
-          break
-     for s in sen:
-          if s == '(': # 열었음
-               open_state.append(')')
-          elif s == '[':
-               open_state.append(']')
-          elif s == ')' or s == ']':
-               if not open_state:
-                    open_state.append('fail')
-                    break
-               else:
-                    if open_state[-1] == s:
-                         open_state.pop(-1)
-                    else:
-                         break
-     if len(open_state) == 0:
-          print('yes')
-     else:
-          print('no')
+    sentence = input().strip('\n')
+    answer = 'yes'
+    lst = [] # 괄호 여부 확인
+    if len(sentence)==1 and sentence[0] == '.':
+        break
+    for s in sentence:
+        if s == '(':
+            lst.append(s)
+
+        elif s == '[':
+            lst.append(s)
+        
+        elif s == ')':
+            if lst and lst[-1] == '(':
+                lst.pop(-1)
+            else:
+                answer = 'no'
+                break
+        elif s == ']':
+            if lst and lst[-1] == '[':
+                lst.pop(-1)
+            else:
+                answer = 'no'
+                break
+    if len(lst) > 0:
+        answer = 'no'
+    print(answer)
