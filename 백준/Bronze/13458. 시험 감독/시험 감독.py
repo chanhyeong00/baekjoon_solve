0e10f1293533fe759a899137d17df52155d59308
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-02-24 21:39:38 +0900

    [Bronze II] Title: 시험 감독, Time: 756 ms, Memory: 132864 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -5,13 +5,10 @@ N = int(input())
 A = list(map(int, input().split()))
 B, C = map(int, input().split())
 num = len(A)
-for i in range(len(A)): # 총 감독관 투입
-    if A[i] - B <= 0:
-        A[i] = 0
-    else:
-        A[i] = A[i]-B
 
-for i in range(len(A)): # 부감독관 투입
+for i in range(len(A)):
+    A[i] -= B # 총감독관 투입
+     # 부감독관 투입
     if A[i] > 0: # 남은 학생이 있는 경우 부감독관 투입 수 계산
         k = A[i] // C
         rem_k = A[i] % C
