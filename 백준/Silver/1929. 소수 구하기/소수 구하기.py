527074d3a0a1ac3d1bb2ad24ebcbec74711e2ad1
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-02-28 06:35:23 +0900

    [Silver III] Title: 소수 구하기, Time: 348 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,12 +1,15 @@
-m, n = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-for num in range(m, n+1):
-    flag = True
-    if num == 1:
+M, N = map(int, input().split())
+arr = [1]*(N + 1)
+arr[0], arr[1] = 0, 0
+for i in range(2, int(N**(1/2)) + 1):
+    if arr[i] == 0:
         continue
-    for j in range(2,int(num**0.5)+1):
-        if num % j == 0:
-            flag = False
-            break
-    if flag == True:
-        print(num)
+    for j in range(2*i, N+1, i):
+        if arr[j] != 0:
+            arr[j] = 0
+for i in range(M, N+1):
+    if arr[i] != 0:
+        print(i)
