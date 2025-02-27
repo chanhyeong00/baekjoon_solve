04bc10b6c0347306ecfdff27bb5097502f778463
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-02-28 04:25:29 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,14 +1,17 @@
 import sys
-i = 0 # 기준인 애 인덱스
-n, k = map(int, sys.stdin.readline().split())
-stack = [i for i in range(1,n+1)]
+input = sys.stdin.readline
 
+n, k = map(int, input().split())
+
+lst = [i for i in range(1, n+1)]
+i = 0 # 기준
 print('<', end='')
-while stack: # 다 빠질 떄까지
+while lst:
     i += (k - 1)
-    while i > (len(stack) - 1): #
-        i -= (len(stack))
-    if len(stack) == 1:
-        print(stack.pop(), end='>\n')
-        break
-    print(stack.pop(i), end=', ')
+
+    while len(lst) <= i:
+        i -= len(lst)
+    if len(lst) == 1:
+        print(lst.pop(i), end='>')
+    else:
+        print(lst.pop(i), end=', ')
