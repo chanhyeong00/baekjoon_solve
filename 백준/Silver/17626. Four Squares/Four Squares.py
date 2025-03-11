81399c1af274639c8a5320dd92d733b1fc72adab
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-03-11 20:04:38 +0900

    [Silver III] Title: Four Squares, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,22 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-max_sqrt = int((n**(1/2)))
-
-dp = [i for i in range(n+1)] # 모두 1^2으로 구성되는 최악의 상황으로 초기화
-
-for i in range(2, n+1):
-    for j in range(1, int(i**(1/2))+1):
-        dp[i] = min(1 + dp[i-j**2], dp[i])
-
-print(dp[n])
-
-# dp[1] = 1
-# dp[2] = dp[1] + dp[1]
-# dp[3] = dp[1] + dp[2]
-# dp[4] = 1
-# dp[5] = dp[4] + dp[1]
-# dp[6] = dp[4] + dp[2]
-# dp[7] = dp[4] + dp[3]
-# dp[n] = dp[i*i] + dp[n-i*i] (dp[i^2]은 항상 1)
+N = int(input())
+# 모든 수는 4개 이하는 제곱근으로 이루어질 수 있다는 걸 생각하자
+def find_min(n):
+    if n ** 0.5 == int(n**0.5): 
+        return 1
+    for i in range(1, int(n**0.5)+1):
+        if int((n - i**2)**0.5) == (n - i**2)**0.5: # 남은 수가 제곱근이면
+            return 2
+    for i in range(1, int(n**0.5) + 1):
+        for j in range(1, int((n-i**2)**0.5)+1):
+            if int((n - i**2 - j**2)**0.5) == (n - i**2 - j**2)**0.5:
+                return 3
+    return 4
+print(find_min(N))
