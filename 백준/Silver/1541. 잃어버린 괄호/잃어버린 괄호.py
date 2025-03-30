2795d9aa1d1f2a0fdefabcc9baed613d08612590
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-03-30 19:12:20 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,16 +1,26 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-input = stdin.readline
+eq = input().strip('\n')
+cnt, answer = 0, 0
+minus_on = -1
 
-form =  input().strip().split('-') # -가 나온 순간부터 다 뻄
+# 55 +  0 1 2     cnt == 2 -> 2 - 2 : 2-1
+for i in range(len(eq)):
+    cnt += 1
+    if  i == len(eq) - 1:
+        if minus_on == 1:
+            answer -= int(eq[i-cnt+1:i+1])
+        else:
+            answer += int(eq[i-cnt+1:i+1])
+    if eq[i] == '-' or eq[i] == '+':
+        if minus_on == -1:
+            if eq[i] == '-': 
+                minus_on = 1
+            answer += int(eq[i-(cnt-1):i])
 
-answer = 0
-
-for i in form[0].split('+'):
-    answer += int(i)
-
-for i in range(1, len(form)):
-    for j in form[i].split('+'):
-        answer -= int(j)
-
+        else:
+            answer -= int(eq[i-(cnt-1):i])
+        cnt = 0
+ 
 print(answer)
