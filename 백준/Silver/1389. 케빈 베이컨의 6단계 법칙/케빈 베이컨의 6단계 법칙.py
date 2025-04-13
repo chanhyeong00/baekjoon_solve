8dc4206ff2c96d7aafa2adc7ea6ecbf85409ff3b
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-04-13 19:37:26 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 372 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,38 +1,39 @@
-from sys import stdin
-from collections import deque
-input = stdin.readline
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
 
-friends = dict()
-n, m = map(int, input().split())
+# 연결 여부
+connect = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for _ in range(M):
+    x, y = map(int, input().split())
+    connect[x-1][y-1] = 1
+    connect[y-1][x-1] = 1
 
-for _ in range(m):
-    f1, f2 = map(int, input().split())
-    if f1 not in friends:
-        friends[f1] = [f2]
-    else:
-        friends[f1].append(f2)
-    if f2 not in friends:
-        friends[f2] = [f1]
-    else:
-        friends[f2].append(f1)
+# 케빈 베이컨 수 저장할 그래프
+graph = [[float('inf')] * N for _ in range(N)]
 
-answer = []
-people = [i for i in range(1, n+1)]
-for p1 in people: # 한명 기준 잡고
-    num = 0 # 베이컨 수
-    for p2 in people:
-        if p1 != p2: # 다른 사람인 경우 이사람까지의 베이컨의 수 구함
-            queue = deque([(p1, 0)])
-            visited = [0] * (n+1)
-            while queue:
-                node, cnt = queue.popleft()
-                if node == p2:
-                    num += cnt
-                    break
-                if not visited[node]:                 
-                    visited[node] = 1
-                    for p3 in friends[node]:
-                        queue.append((p3, cnt+1))
-    answer.append((p1, num))
-answer.sort(key=lambda x:[x[1], x[0]])
-print(answer[0][0])
+# 직행 노선 구하기
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            graph[i][j] = 0
+        else:
+            if connect[i][j] == 1:
+                graph[i][j] = 1
+
+# 경유 노선 
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+# 답
+bacon = float('inf')
+answer = 0
+
+for i in range(N):
+    b = sum(graph[i])
+    if b < bacon:
+        answer= i
+        bacon = b
+print(answer+1)
