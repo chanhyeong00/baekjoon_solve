88c3d628a12726814261f74ddaa870229ef84dec
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-04-28 21:15:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,47 +1,30 @@
-from sys import stdin
-from collections import deque
-input = stdin.readline
+import sys
+input = sys.stdin.readline
 
-
-n = int(input())
+N = int(input())
 vil = []
-for _ in range(n):
-    vil.append(list(input()))
+for _ in range(N):
+    vil.append(input())
 
-visited = [[0] * n for _ in range(n)]
-d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-house_num = 0
+visited = [[0]*N for _ in range(N)]
+dxy = [(1,0),(-1,0),(0,1),(0,-1)]
 answer = []
-num = 0 # 집 번호
-while True:
-    x, y = -1, -1
-    flag = False
-    for i in range(n): # 집을 찾는다.
-        for j in range(n):
-            if vil[i][j] == '1' and not visited[i][j]:
-                y, x = i, j
-                flag = True
-                break
-        if flag == True: break
-    if flag == False: break # 찾을 집이 없음을 의미함
-    # 집이 있다면 탐색 시작
-    num += 1
-    cnt = 0
-    queue = deque([(y, x)]) # 좌표, 현재단지번호상태, 그곳 단지번호 
-    while queue:
-        y, x = queue.popleft()
-        if not visited[y][x]:
-            cnt += 1
-            visited[y][x] = 1
-            for i in range(4):
-                ny, nx = y+d[i][0], x+d[i][1]
-                if 0<=ny<n and 0<=nx<n and vil[ny][nx] == '1'\
-                    and not visited[ny][nx]: # 단지 찾기
-                        queue.append((ny, nx))
-        
-    answer.append(cnt)
+for i in range(N):
+    for j in range(N):
+        cnt = 1
+        if not visited[i][j] and vil[i][j] == '1':
+            stack = [(i, j)]
+            visited[i][j] = 1
+            while stack:
+                x, y = stack.pop()
+                for dx, dy in dxy:
+                    nx, ny = x+dx, y+dy
+                    if 0<=nx<N and 0<=ny<N and vil[nx][ny] == '1' and not visited[nx][ny]:
+                        visited[nx][ny] = 1
+                        stack.append((nx, ny))
+                        cnt += 1
+            answer.append(cnt)
+print(len(answer))
 answer.sort()
-print(num)
 for a in answer:
     print(a)
