ec0d5dca550842fe4bc30e087a68600359917dbd
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-06-09 22:34:29 +0900

    [Silver I] Title: 경로 찾기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,33 +1,27 @@
-from sys import stdin
-input = stdin.readline
+import sys
 
 N = int(input())
-
-graph = [] # 그래프의 간선 상태
-line = dict()
-
+graph = []
+connection = dict()
 for _ in range(N):
     graph.append(list(map(int, input().split())))
 
 for i in range(N):
-    line[i] = []
+    connection[i] = []
     for j in range(N):
         if graph[i][j] == 1:
-            line[i].append(j)
-
+            connection[i].append(j)
 for i in range(N):
-    start = i # 시작점
-    stack = [] # 갈 수 있는 노드 리스트
-    for k in line[i]:
-        stack.append(k)
+    visited = [0]*N
+    stack = []
+    start = i
+    stack.append(i)
     while stack:
-        end = stack.pop()
-        graph[start][end]=1 # 갈 수 있는 노드임을 표시
-        for k in line[end]:
-            if graph[start][k] == 0: # 갔던 곳 아니면 넣기
-               stack.append(k)
-
-for i in range(N):
-    for j in range(N):
-        print(graph[i][j], end=' ')
-    print()
+        node = stack.pop()
+        for k in connection[node]:
+            if not visited[k]:
+                graph[start][k] = 1
+                visited[k] = 1
+                stack.append(k)
+for g in graph:
+    print(*g)
