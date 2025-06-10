7612a4bf37b54791879c94427466adf7718be9db
chanhyeong00 <156037786+chanhyeong00@users.noreply.github.com>
2025-06-10 20:47:40 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 704 ms, Memory: 43552 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,48 +1,42 @@
-from sys import stdin
+import sys
 from collections import deque
-
-input = stdin.readline
-
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = []
-visited = [[0] * m for _ in range(n)]
+
+map_ = []
+dis_lst = [[0]*m for _ in range(n)]
 for _ in range(n):
-    g = list(map(int, input().split()))
-    graph.append(g)
+    map_.append(list(map(int, input().split())))
 
-# 목표를 찾고 인접노드부터 차례로 살핀다.
-x, y = 0, 0 # 목표 지점 위치
+# 2 지점 먼저 찾기
+dest_i, dest_j = 0, 0
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 2:
-            x, y = i, j
-            break
-        
-queue = deque([(x,y,0)])
-distance = [[0] * m for _ in range(n)]
+        if map_[i][j] == 2:
+            dest_i, dest_j = i, j
 
+# 2 지점부터 너비 우선 탐색으로 거리 찾기
+dxy = [(0,1), (0, -1), (1, 0), (-1, 0)]
+visited = [[0]*m for _ in range(n)]
+
+queue = deque([(dest_i, dest_j, 0)])
+visited[dest_i][dest_j] = 1
 while queue:
-    row, col, dist = queue.popleft()
-    if row < 0 or row >= n or col < 0 or col >= m or \
-        visited[row][col]: # 방문 했거나 인덱스 벗어나면
-        continue
-    
-    if graph[row][col] != 0: # 방문 안했고 밟을 수 있는 땅이면
-        queue.append((row-1, col, dist+1))
-        queue.append((row+1, col, dist+1))
-        queue.append((row, col-1, dist+1))
-        queue.append((row, col+1, dist+1))
-        # 거리 표시
-        distance[row][col] = dist
-
-    visited[row][col] = 1 # 방문했음
-
+    x, y, dis = queue.popleft()
+    for (dx, dy) in dxy:
+        nx, ny = x+dx, y+dy
+        if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and map_[nx][ny] != 0:
+            queue.append((nx, ny, dis+1))
+            visited[nx][ny] = 1
+            dis_lst[nx][ny] = dis + 1
+        
+# 확인 작업(지도에서 원래 0이면 0, 0이 아닌데 0이면 -1로)
 for i in range(n):
     for j in range(m):
-        if visited[i][j] == 0 and graph[i][j] != 0:
-            distance[i][j] = -1
+        # if map_[i][j] == 0:
+        #     dis_lst[i][j] = 0
+        if map_[i][j] !=0 and visited[i][j] == 0:
+            dis_lst[i][j] = -1
 for i in range(n):
-    for j in range(m):
-        print(distance[i][j], end=' ')
-    print()
+    print(*dis_lst[i])
